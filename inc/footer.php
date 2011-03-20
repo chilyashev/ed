7d07fd60a86ba986a176e3d1187b2d3250f297de
Mihail Chilyashev <chilyashev@gmail.com>
2011-03-20 18:20:24 +0200

    loading time & stuff

--- inc/footer.php
+++ inc/footer.php
@@ -7,14 +7,12 @@
 <p style="font-size: 0.8em;">Powered by Електронен дневник <?=$version?><br />
 © <a href="http://chilyashev.com" target="_blank">Mihail Chilyashev</a> 2011<br />
 <?
-$mtime = microtime();
-$mtime = explode(" ",$mtime);
-$mtime = $mtime[1] + $mtime[0];
-
-$tend = $mtime;
-$totaltime = ($tend - $tstart);
-
-printf ("Страницата зареди за %f секунди.", $totaltime);
+$endtime = microtime();
+$endarray = explode(" ", $endtime);
+$endtime = $endarray[1] + $endarray[0];
+$totaltime = ($endtime - $starttime);
+$totaltime = round($totaltime,6);
+printf("Страницата зареди за %f секунди.", $totaltime);
 ob_flush();
 ?> </p></div></div>
 </div>
