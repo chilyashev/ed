db03ea175e89af6b2b9d1f2eca5c1a085ae762e7
Mihail Chilyashev <chilyashev@gmail.com>
2011-03-31 01:25:48 +0300

    stats almost ready. TODO: fix grades edit/del

--- admin/stats.php
+++ admin/stats.php
@@ -1,14 +1,35 @@
 <?
 include "admheader.php";
-?><script>actadmlink("#stats")</script>
+include_once 'stats/ofc-library/open_flash_chart_object.php';
+?>
 <h2>&raquo;Статистики</h2><br />
 <br /><br />
+<script type="text/javascript">actadmlink("#stats")</script>
 
-
+<!--<table width="" border="0" cellspacing="0" cellpadding="0">
+		<?
+		/*for($i = 6; $i > 1;$i--){
+$q = mysql_query("SELECT * FROM `ocenka` WHERE `value` like '$i%'");
+while($r = mysql_fetch_array($q)){
+	
+	}
+		echo"
+			<tr style=\"border-bottom:1px;\">
+				<td width=\"100px\">$i</td>
+				<td>".((cnt("ocenka", "WHERE `value` like '$i%'") / cnt("ocenka"))*100)."%</td>
+		</tr>
+			";
+			}*/
+			
+		?>
+</table>-->
 <?
-echo cnt("ocenka");
-?>
+open_flash_chart_object( 360, 250, get_option("url").'admin/stats/ocenki.php', false );
+open_flash_chart_object( 360, 250, get_option("url").'admin/stats/ocenkip.php', false );
+echo "<br /><br />";
+open_flash_chart_object( 250, 250, get_option("url").'admin/stats/abs.php', false );
 
+?>
 <?
 include "footer.php";
-?>
+?>a
